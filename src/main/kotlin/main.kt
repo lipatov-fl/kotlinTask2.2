b3d589ba2695d9b0824091a8d6fef4046a169305
lipatov-fl <lipatov-fl@yandex.ru>
2022-06-11 23:38:53 +0400

    Добавил .gitirnore и решение второй задачи

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,10 @@
+package ru.netology
+
+fun main() {
+    val likes = 20
+    val likesStringLength = likes.toString().length
+    val penultimateSymbol = if (likesStringLength > 1) likes.toString()[likesStringLength - 2] else '0'
+    val ending = if (likes % 10 == 1 && penultimateSymbol != '1') "человеку" else "людям"
+
+    println("Понравилось: $likes $ending")
+}
